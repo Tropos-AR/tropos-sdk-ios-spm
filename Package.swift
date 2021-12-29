778287510d1953702603fc47a1ea7d1bf33b6c59
Vesna Farmak <vesna.farmak@tropos.ar>
2021-12-29 15:07:42 +0100

    update package

--- Package.swift
+++ Package.swift
@@ -4,29 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "TroposAR",
+    name: "TroposARSDK",
+    platforms: [
+           .iOS(.v11)
+       ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "TroposAR",
-            targets: ["tropos-ar-ios-spm","TroposARSDK"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+       .library(
+            name: "TroposARSDK",
+            targets: ["TroposARSDK"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "tropos-ar-ios-spm",
-            dependencies: []),
-        .binaryTarget(
+           .binaryTarget(
                    name: "TroposARSDK",
                    path: "lib/TroposARSDK.xcframework" // this is a symlink
-               ),
-        .testTarget(
-            name: "tropos-ar-ios-spmTests",
-            dependencies: ["tropos-ar-ios-spm"]),
+               )
     ]
 )
