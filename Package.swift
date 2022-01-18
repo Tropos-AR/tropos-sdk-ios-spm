d2b8092132628deadcc9b321d6e7e5ec0adb8313
Vesna Farmak <vesna.farmak@tropos.ar>
2022-01-18 14:25:44 +0100

    add troposARSDK

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "tropos-ar-ios-spm",
     platforms: [
-           .iOS(.v11)
+           .iOS(.v13)
        ],
     products: [
        .library(
--- Sources/tropos-ar-ios-spm/tropos_ar_ios_spm.swift
+++ Sources/tropos-ar-ios-spm/tropos_ar_ios_spm.swift
@@ -1,7 +1,6 @@
 import UnityFramework
 import CrashReporter
-
-
+import TroposARSDK
 
 public struct tropos_ar_ios_spm {
     public private(set) var text = "Hello, World!"
